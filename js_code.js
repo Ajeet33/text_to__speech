6e546a19bec7d35cb2ebbec873d268011e3e35bb
ajeet <ajeetmcmt@gmail.com>
2025-07-28 21:57:40 +0530

    Fix: voice list bug and updated code

--- js_code.js
+++ js_code.js
@@ -3,12 +3,13 @@ let speech = new SpeechSynthesisUtterance();
 let voices = [];
 let voiceSelect = document.querySelector("select");
 
-window.SpeechSynthesis.onvoiceschanged = () => {
+window.speechSynthesis.onvoiceschanged = () => {
     voices = window.speechSynthesis.getVoices();
     speech.voice = voices[0];
 
-    voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)
-))};
+    voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.
+        name, i)));
+};
 voiceSelect.addEventListener("change", () =>{
     speech.voice = voices[voiceSelect.value];
 });
